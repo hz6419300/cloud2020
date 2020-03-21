d6fa331efce7c30690809f32cb8ffb561e4c2ec9
hz6419300 <hz568989902@163.com>
2020-03-21 18:22:51 +0800

    第二次提交

---
+++ seata-account-service2003/src/main/java/com/atguigu/springcloud/alibaba/service/AccountService.java
@@ -0,0 +1,16 @@
+package com.atguigu.springcloud.alibaba.service;
+
+import java.math.BigDecimal;
+
+public interface AccountService {
+
+
+    /**
+     * 减库存
+     *
+     * @param userId 用户id
+     * @param money  金额
+     * @return
+     */
+    void decrease(Long userId, BigDecimal money);
+}
